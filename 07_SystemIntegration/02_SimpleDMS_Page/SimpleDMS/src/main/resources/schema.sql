eea975da7a66ac6d921357c74598a37c7ab4e9c9
songkidong <141198018+songkidong@users.noreply.github.com>
2023-11-06 17:32:13 +0900

    SI

--- 07_SystemIntegration/02_SimpleDMS_Page/SimpleDMS/src/main/resources/schema.sql
+++ 07_SystemIntegration/02_SimpleDMS_Page/SimpleDMS/src/main/resources/schema.sql
@@ -88,7 +88,7 @@ CREATE TABLE TB_EMP
     DELETE_TIME VARCHAR2(255)
 );
 
--- 코드성 테이블 : 공통 코드 유형 테이블
+-- 코드성 테이블 : 공통 코드 유형(분류) 테이블
 CREATE TABLE TB_CODE_CATEGORY
 (
     CATEGORY_ID   NUMBER NOT NULL
@@ -289,4 +289,3 @@ CREATE TABLE TB_GALLERY
     UPDATE_TIME       VARCHAR2(255),
     DELETE_TIME       VARCHAR2(255)
 );
-
